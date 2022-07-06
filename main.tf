f223c9acbbcd2b1a541f16f16cd78d3a977a3a23
Ashwin Jacob <ashjacob@gmail.com>
2022-07-05 20:26:42 -0500

    SRE-6 | Remove the star

--- main.tf
+++ main.tf
@@ -415,7 +415,7 @@ resource "aws_codepipeline_webhook" "webhook" {
   name            = module.codepipeline_label.id
   authentication  = var.webhook_authentication
   target_action   = var.webhook_target_action
-  target_pipeline = join("", aws_codepipeline.default.*.name)
+  target_pipeline = join("", aws_codepipeline.default.name)
 
   authentication_configuration {
     secret_token = local.webhook_secret
